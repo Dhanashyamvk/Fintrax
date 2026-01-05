499ddd5fbcacc6c43995122a2519bb6f88bf0b5f
DHANASHYAM V K <dhanashyamvk@gmail.com>
2026-01-05 21:30:23 +0530

    Delete test.js

--- test.js
+++
@@ -1,7 +0,0 @@
-const { extractAndParseBill } = require("./bill");
-
-extractAndParseBill({ imagePath: "./samplebill.png" })
-  .then(result => {
-    console.log(result.parsed);
-  })
-  .catch(err => console.error(err));
